cbc5977f92b8b37cf4e6e36cb707f40ec6325399
Nikolas Corp <nikolasmvsantos@gmail.com>
2021-07-23 09:58:38 -0300

    CRIANDO TELA DE LOGIN

---
+++ DASHBOARD/LOGIN/index.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Login</title>
+		<link rel="stylesheet" type="text/css" href="CSS/estilo.css">
+	</head>
+
+	<body>
+		<div id="corpo-form">
+			<h1>Login Dashboard</h1>
+			<form method="POST" action="processa.php">
+			<input type="login" placeholder=" Usuario">
+			<input type="password" placeholder=" Senha">
+			<input type="submit" value="Acessar">
+			<a href="troca_senha.php">Esqueceu a senha?<strong>Clique Aqui!</strong></a>
+		</div>
+	</body>
+</html>
---
+++ DASHBOARD/LOGIN/troca_senha.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Troca de Senha</title>
+		<link rel="stylesheet" type="text/css" href="CSS/estilo.css">
+	</head>
+
+	<body>
+		<div id="corpo-form">
+			<h1>Trocar a Senha</h1>
+			<form method="POST" action="processa.php">
+			<input type="Login" placeholder=" Usuario">
+			<input type="password" placeholder=" Nova Senha">
+			<input type="password" placeholder=" Confirmar Nova Senha">
+			<input type="submit" value="Alterar">
+		</div>
+	</body>
+</html>
