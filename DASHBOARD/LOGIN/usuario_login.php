04aa17c4ffd837bd008e49089682afb0599a8e6f
Nikolas Corp <nikolasmvsantos@gmail.com>
2021-07-26 15:07:15 -0300

    REQUISIÇÃO DE LOGIN

---
+++ DASHBOARD/LOGIN/usuario_login.php
@@ -0,0 +1,27 @@
+<?php 
+	require_once ("../CONEXAO SQL/config.php");
+	require_once ("../CONEXAO SQL/classe/sql.php");
+	require_once ("../CONEXAO SQL/classe/usuario.php");
+
+	$user=new Usuario;
+
+	if (isset($_POST['usuario']))
+	{
+		$usuario = addslashes($_POST['usuario']);
+		$senha = addslashes($_POST['senha']);
+
+		if (isset($usuario))
+		{
+			$user->login($usuario,$senha);
+		}
+		else
+		{
+			$user->Exception();
+		}
+	}
+	else
+	{
+
+	}
+
+?>
