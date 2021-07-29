b372c05a36dc2fde16e192d463cd669c002237ed
Nikolas Corp <nikolasmvsantos@gmail.com>
2021-07-29 15:44:37 -0300

    ORGANIZANDO O CÓDIGO

--- DASHBOARD/index.php
+++ DASHBOARD/index.php
@@ -1,13 +1,13 @@
 <html>
    <head>
+      <title>DASHBOARD HOSPITALAR</title>
       <link rel="stylesheet"  href="CSS\GRAFICO_1.css" /> 
       <link rel="stylesheet"  href="CSS\GRAFICO_2.css" /> 
       <link rel="stylesheet"  href="CSS\GRAFICO_3.css" />
-      <title>DASHBOARD HOSPITALAR</title>
    </head>
 
    <body>
-     <hr class="hr3"></hr>
+      <hr class="hr3"></hr>
       <figure class="highcharts-figure">
          <div id="container1"></div>
          <hr class="hr3"></hr>
@@ -15,14 +15,14 @@
          <hr class="hr3"></hr>
          <div id="container3"></div>
       </figure>
-  
-     <script src="https://code.highcharts.com/highcharts.js"></script>
-     <script src="https://code.highcharts.com/modules/export-data.js"></script>
-     <script src="https://code.highcharts.com/modules/accessibility.js"></script>
-     <script src="https://code.highcharts.com/modules/exporting.js"></script>
 
-     <script src="JS\GRAFICO_1.js"></script>
-     <script src="JS\GRAFICO_2.js"></script>
-     <script src="JS\GRAFICO_3.js"></script>
+      <script src="https://code.highcharts.com/highcharts.js"></script>
+      <script src="https://code.highcharts.com/modules/export-data.js"></script>
+      <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+      <script src="https://code.highcharts.com/modules/exporting.js"></script>
+      
+      <script src="JS\GRAFICO_1.js"></script>
+      <script src="JS\GRAFICO_2.js"></script>
+      <script src="JS\GRAFICO_3.js"></script>
    </body>
 </html>
