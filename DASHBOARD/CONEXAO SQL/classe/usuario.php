5d034f4ebb580f7f1b365847cc95d4dc3be17b76
Nikolas Corp <nikolasmvsantos@gmail.com>
2021-08-09 16:40:05 -0300

    ALTERADO O MODO DE BUSCA DO USUARIO E ORGANIZAÇÃO DO CODIGO

--- DASHBOARD/CONEXAO SQL/classe/usuario.php
+++ DASHBOARD/CONEXAO SQL/classe/usuario.php
@@ -6,43 +6,41 @@ class Usuario
 	private $nome;
 	private $login;
 	private $senha;
-
+/*------------------------------------------*/
 	public function getId(){
 		return $this->id;
 	}
 	public function setId($value){
 		$this->id= $value;
 	}
-
-
+/*------------------------------------------*/
 	public function getNome(){
 		return $this->nome;
 	}
 	public function setNome($value){
 		$this->nome= $value;
 	}
-
+/*------------------------------------------*/
 	public function getLogin(){
 		return $this->login;
 	}
 	public function setLogin($value){
 		$this->login= $value;
 	}
-
-
+/*------------------------------------------*/
 	public function getSenha(){
 		return $this->senha;
 	}
 	public function setsenha($value){
 		$this->senha= $value;
 	}
+/*------------------------------------------*/
 
-
-
-	public function loadById($id)
+	//CARREGA O LOGIN (NÚMERO) DO USUARIO
+	public function loadByLogin($login)
 	{
 		$sql =new Sql();
-		$results=$sql->select("SELECT*FROM usuario WHERE id= :ID",array(":ID"=>$id));
+		$results=$sql->select("SELECT*FROM usuario WHERE login= :LOGIN",array(":LOGIN"=>$login));
 
 		if (count($results) > 0){
 
@@ -50,42 +48,45 @@ class Usuario
 		}
 	}
 
-	public static function getList(){
 
-		$sql =new Sql();
-		return $sql->select("SELECT *FROM tb_usuarios ORDER BY deslogin;");
-	}
-
-
-	public function logar($login,$senha){
+	//FAZ O LOGIN DO USUARIO
+	public function login($login,$senha)
+	{
 		$sql =new Sql();
 		$results=$sql->select("SELECT*FROM usuario WHERE login= :LOGIN AND senha = :SENHA",array(":LOGIN"=>$login, ":SENHA"=>$senha));
 
+		//VALIDA SE TEM REGISTRO NO BANCO DE DADOS
 		if (count($results) > 0){
 			$this->setData($results[0]);
-		} 
+		}
+		//SE NÃO TIVER, RETORNA ERRO.
 		else {
 			throw new Exception ("Login e/ou senha inválidos.");
 		}
 	}
 
-	public function setData($dados){
+	public function setData($dados)
+	{
 		$this->setId($dados['id']);
 		$this->setLogin($dados['login']);
 		$this->setSenha($dados['senha']);
 		$this->setNome($dados['nome']);
 	}
 
-
-	public function update($senha){
+	//FAZ A ATUALIZAÇÃO DE SENHA DO USUARIO
+	public function update($senha)
+	{
 		$this->setSenha($senha);
 
 		$sql = new Sql();
 		$sql->query("UPDATE usuario SET senha= :SENHA WHERE login=:LOGIN",array(':SENHA'=>$this->getSenha(),':LOGIN'=>$this->getLogin()));
 	}
 	
-	public function __toString(){
-	return json_encode(array(
+
+	//TRANSFORMA EM UM JSON O RESULTADO APRESENTADO NA TELA
+	public function __toString()
+	{
+		return json_encode(array(
 		"id"=>$this->getId(),
 		"login"=>$this->getLogin(),
 		"senha"=>$this->getSenha(),
