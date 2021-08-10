aa13e194396c40f7e04982956d6df04dad39cd53
Nikolas Corp <nikolasmvsantos@gmail.com>
2021-08-10 14:50:23 -0300

    AJUSTADO NOME DA VARIAVEL

--- DASHBOARD/CONEXAO SQL/classe/usuario.php
+++ DASHBOARD/CONEXAO SQL/classe/usuario.php
@@ -1,98 +1,101 @@
 <?php 
 
-class Usuario 
-{
-	private $id;
-	private $nome;
-	private $login;
-	private $senha;
-/*------------------------------------------*/
-	public function getId(){
-		return $this->id;
-	}
-	public function setId($value){
-		$this->id= $value;
-	}
-/*------------------------------------------*/
-	public function getNome(){
-		return $this->nome;
-	}
-	public function setNome($value){
-		$this->nome= $value;
-	}
-/*------------------------------------------*/
-	public function getLogin(){
-		return $this->login;
-	}
-	public function setLogin($value){
-		$this->login= $value;
-	}
-/*------------------------------------------*/
-	public function getSenha(){
-		return $this->senha;
-	}
-	public function setsenha($value){
-		$this->senha= $value;
-	}
-/*------------------------------------------*/
+	require_once ("sql.php");
 
-	//CARREGA O LOGIN (NÚMERO) DO USUARIO
-	public function loadByLogin($login)
+	class Usuario 
 	{
-		$sql =new Sql();
-		$results=$sql->select("SELECT*FROM usuario WHERE login= :LOGIN",array(":LOGIN"=>$login));
-
-		if (count($results) > 0){
-
-			$this->setData($results[0]);
+		private $id;
+		private $nome;
+		private $login;
+		private $senha;
+	 /*------------------------------------------*/
+		public function getId(){
+			return $this->id;
 		}
-	}
-
-
-	//FAZ O LOGIN DO USUARIO
-	public function login($login,$senha)
-	{
-		$sql =new Sql();
-		$results=$sql->select("SELECT*FROM usuario WHERE login= :LOGIN AND senha = :SENHA",array(":LOGIN"=>$login, ":SENHA"=>$senha));
-
-		//VALIDA SE TEM REGISTRO NO BANCO DE DADOS
-		if (count($results) > 0){
-			$this->setData($results[0]);
-			session_start();
+		public function setId($value){
+			$this->id= $value;
 		}
-		//SE NÃO TIVER, RETORNA ERRO.
-		else {
-			throw new Exception ("Login e/ou senha inválidos.");
+	 /*------------------------------------------*/
+		public function getNome(){
+			return $this->nome;
 		}
-	}
+		public function setNome($value){
+			$this->nome= $value;
+		}
+	 /*------------------------------------------*/
+		public function getLogin(){
+			return $this->login;
+		}
+		public function setLogin($value){
+			$this->login= $value;
+		}
+	 /*------------------------------------------*/
+		public function getSenha(){
+			return $this->senha;
+		}
+		public function setsenha($value){
+			$this->senha= $value;
+		}
+ 	 /*------------------------------------------*/
 
-	public function setData($dados)
-	{
-		$this->setId($dados['id']);
-		$this->setLogin($dados['login']);
-		$this->setSenha($dados['senha']);
-		$this->setNome($dados['nome']);
-	}
+		//CARREGA O LOGIN (NÚMERO) DO USUARIO
+		public function loadByLogin($id)
+		{
+			$sql =new Sql();
+			$results=$sql->select("SELECT*FROM usuario WHERE login= :LOGIN",array(":LOGIN"=>$id));
 
-	//FAZ A ATUALIZAÇÃO DE SENHA DO USUARIO
-	public function update($senha)
-	{
-		$this->setSenha($senha);
+			if (count($results) > 0){
 
-		$sql = new Sql();
-		$sql->query("UPDATE usuario SET senha= :SENHA WHERE login=:LOGIN",array(':SENHA'=>$this->getSenha(),':LOGIN'=>$this->getLogin()));
-	}
+				$this->setData($results[0]);
+			}
+		}
+
+
+		//FAZ O LOGIN DO USUARIO
+		public function login($usuario,$senha)
+		{
+			$sql =new Sql();
+			$results=$sql->select("SELECT*FROM usuario WHERE login= :LOGIN AND senha = :SENHA",array(":LOGIN"=>$usuario, ":SENHA"=>md5($senha)));
+
+			//VALIDA SE TEM REGISTRO NO BANCO DE DADOS
+			if (count($results) > 0){
+				$this->setData($results[0]);
+				session_start();
+				$_SESSION['id_usuario']="true";
+			}
+
+			//SE NÃO TIVER, RETORNA ERRO.
+			else {
+				throw new Exception ("Login e/ou senha inválidos.");
+			}
+		}
+
+		public function setData($dados)
+		{
+			$this->setId($dados['id']);
+			$this->setLogin($dados['login']);
+			$this->setSenha($dados['senha']);
+			$this->setNome($dados['nome']);
+		}
+
+		//FAZ A ATUALIZAÇÃO DE SENHA DO USUARIO
+		public function update($senha)
+		{
+			$this->setSenha(md5($senha));
+
+			$sql = new Sql();
+			$sql->query("UPDATE usuario SET senha= :SENHA WHERE login=:LOGIN",array(':SENHA'=>$this->getSenha(),':LOGIN'=>$this->getLogin()));
+		}
 	
 
-	//TRANSFORMA EM UM JSON O RESULTADO APRESENTADO NA TELA
-	public function __toString()
-	{
-		return json_encode(array(
-		"id"=>$this->getId(),
-		"login"=>$this->getLogin(),
-		"senha"=>$this->getSenha(),
-		"nome"=>$this->getNome()));
+		//TRANSFORMA EM UM JSON O RESULTADO APRESENTADO NA TELA
+		public function __toString()
+		{
+			return json_encode(array(
+			"id"=>$this->getId(),
+			"login"=>$this->getLogin(),
+			"senha"=>$this->getSenha(),
+			"nome"=>$this->getNome()));
+		}
 	}
-}
-
 ?>
