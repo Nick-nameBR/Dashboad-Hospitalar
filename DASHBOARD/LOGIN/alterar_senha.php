8dc95c553fb3ece2d2000cf21953a6dbc77487b7
Nikolas Corp <nikolasmvsantos@gmail.com>
2021-08-10 17:21:28 -0300

    CRIANDO MENSAGEM DE CAPSLOCK

--- DASHBOARD/LOGIN/alterar_senha.php
+++ DASHBOARD/LOGIN/alterar_senha.php
@@ -6,12 +6,23 @@
 	</head>
 
 	<body>
+		<script language="Javascript">
+			function capLock(e){
+ 				kc = e.keyCode?e.keyCode:e.which;
+ 				sk = e.shiftKey?e.shiftKey:((kc == 16)?true:false);
+ 				if(((kc >= 65 && kc <= 90) && !sk)||((kc >= 97 && kc <= 122) && sk))
+  				document.getElementById('divMayus').style.visibility = 'visible';
+ 				else
+  				document.getElementById('divMayus').style.visibility = 'hidden';
+			}
+		</script>
 		<div id="corpo-form">
 			<img id="imagem" src="../logos/logo4.png">
 			<form action="processa.php" method="POST" >
 			<input type="Login" name="usuario" title="Informe o seu usuario" placeholder=" Usuario">
-			<input type="password" onpaste="return false" ondrop="return false" name="senha"  placeholder=" Nova Senha" minlength="6" maxlength="10">
-			<input type="password" onpaste="return false" ondrop="return false" name="confsenha" placeholder=" Confirmar Nova Senha" minlength="6" maxlength="10">
+			<input type="password" onkeypress="capLock(event)" onpaste="return false" ondrop="return false" name="senha"  placeholder=" Nova Senha" minlength="6" maxlength="15">
+			<input type="password" onkeypress="capLock(event)" onpaste="return false" ondrop="return false" name="confsenha" placeholder=" Confirmar Nova Senha" minlength="6" maxlength="15">
+			<div id="divMayus" style="visibility: hidden">Caps Lock Ligado.</div>
 			<input type="submit" title="Clique para confirmar alteração" value="Alterar">
 		</div>
 	</body>
--- DASHBOARD/LOGIN/login.php
+++ DASHBOARD/LOGIN/login.php
@@ -6,11 +6,22 @@
 	</head>
 
 	<body>
+		<script language="Javascript">
+			function capLock(e){
+ 				kc = e.keyCode?e.keyCode:e.which;
+ 				sk = e.shiftKey?e.shiftKey:((kc == 16)?true:false);
+ 				if(((kc >= 65 && kc <= 90) && !sk)||((kc >= 97 && kc <= 122) && sk))
+  				document.getElementById('divMayus').style.visibility = 'visible';
+ 				else
+  				document.getElementById('divMayus').style.visibility = 'hidden';
+			}
+		</script>
 		<div id="corpo-form">
 			<img id="imagem" src="..\logos\logo4.png">
 			<form action="logar.php" method="POST">
-			<input type="login" name="usuario" title="Coloque seu Usuario" placeholder=" Usuario">
-			<input type="password" onpaste="return false" ondrop="return false"  name="senha" title="Informe sua senha" placeholder=" Senha" minlength="6" maxlength="15">
+			<input type="login" name="usuario" title="Digite seu Usuario" placeholder=" Usuario">
+			<input type="password" onkeypress="capLock(event)" onpaste="return false" ondrop="return false"  name="senha" title="Digite sua senha" placeholder=" Senha" minlength="6" maxlength="15" >
+			<div id="divMayus" style="visibility: hidden">Caps Lock Ligado.</div>
 			<input type="submit" value="Acessar">
 			<b>Esqueceu a senha?<b>
 			<a href="alterar_senha.php"><strong>Clique Aqui!</strong></a>
