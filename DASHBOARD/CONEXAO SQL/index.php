c77dd2d0b8cf93ebceffe12bd38b48b832de25f9
Nikolas Corp <nikolasmvsantos@gmail.com>
2021-08-10 14:50:34 -0300

    TESTE DE CONEXÃO COM BD

--- DASHBOARD/CONEXAO SQL/index.php
+++ DASHBOARD/CONEXAO SQL/index.php
@@ -5,16 +5,18 @@
 
 	//CARREGA UM USUARIO LOGIN E SENHA
 	$user = new Usuario();
-	$user->login("22487",md5("nikolas2021"));
-	echo $user;
+	$user->login("22487","nikolas2021"); 
 	echo "<br>";
 	echo "Logado com sucesso!";
 
+
+
+
+
 	//UPDATE DE USUARIO
-	//$user= new Usuario();
-	//$user->loadByLogin(22487);
-	//$user->update(md5("nikolas2021"));
-	//echo $user;
-	//echo "<br>";
-	//echo "Senha alterada com sucesso!";
+	/*$user= new Usuario();
+	$user->loadByLogin(22487);
+	$user->update("nikolas2021");
+	echo "<br>";
+	echo "Senha alterada com sucesso!";*/
 ?>
